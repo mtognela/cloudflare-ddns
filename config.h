0e26bd519a71d4ff0b16c002fd94691ba6956807
Mattia Tognela <tognelamattia@protonmail.com>
2025-09-21 16:26:33 +0200

    licence update

--- config.h
+++ config.h
@@ -1,6 +1,24 @@
+/*
+    Cloudflare Dynamic DNS Updater writter in C
+    Copyright (C) 2025  Mattia Tognela
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
+*/
+
 #ifndef CONFIG_H
 #define CONFIG_H
-
 
 #define AUTH_EMAIL ""
 #define AUTH_METHOD "token"  // "global" for Global API Key or "token" for Scoped API Token
@@ -10,8 +28,5 @@
 #define TTL 3600
 #define PROXY "false"
 #define SITE_NAME ""
-#define SLACK_CHANNEL ""
-#define SLACK_URI ""
-#define DISCORD_URI ""
 
 #endif
