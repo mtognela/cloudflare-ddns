83ce0f8580a5e58bc87f7a06a456839871288619
Mattia Tognela <tognelamattia@protonmail.com>
2025-09-21 15:06:03 +0200

    Config param now in config.h

---
+++ config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#define AUTH_EMAIL ""
+#define AUTH_METHOD "token"  // "global" for Global API Key or "token" for Scoped API Token
+#define AUTH_KEY ""
+#define ZONE_IDENTIFIER ""
+#define RECORD_NAME ""
+#define TTL 3600
+#define PROXY "false"
+#define SITE_NAME ""
+#define SLACK_CHANNEL ""
+#define SLACK_URI ""
+#define DISCORD_URI ""
+
+#endif
